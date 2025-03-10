fb1629fd1fe5d81a5fdbd44af1393a8d7c2354bb
MFUMATTEW <mfumattew2@gmail.com>
2025-03-10 20:13:56 +0100

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your percentage ");
+        string ValueFromUser = Console.ReadLine();
+        int x = int.Parse(ValueFromUser);
+
+        string letter  =  ""; 
+
+        if ( x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if ( x >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you will do better next term!");
+        }
+
     }
 }
