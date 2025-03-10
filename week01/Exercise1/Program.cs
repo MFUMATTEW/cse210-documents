a5f8b7e3586d1da71b2b8947e21b9fd76676fa9f
MFUMATTEW <mfumattew2@gmail.com>
2025-03-10 20:13:25 +0100

    Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your name?");
+        String name = Console.ReadLine();  
+
+        Console.WriteLine($"Your name is {name}, {first_name} {name}.");
     }
 }
