b87e5e62b8d4e09dd5d4fc6c878327c47f18a82c
MFUMATTEW <mfumattew2@gmail.com>
2025-04-11 09:55:53 +0100

    Shapes Program

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,22 @@
+using System.Xml.Serialization;
+
+public class Circle : Shape
+{
+    private double _radius = 0;
+
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+         List<Shape> shapes = new List<Shape>();
+
+        Square eSquare = new Square();
+        eSquare.SetSide(4);
+        eSquare.SetColor ("red"); 
+
+        Rectangle sRectangle = new Rectangle();
+        sRectangle.SetColor("orange");
+        sRectangle.SetLength(5);
+        sRectangle.SetWidth(3);
+
+        Circle eCircle = new Circle();
+        eCircle.SetColor("green");
+        eCircle.SetRadius(6);
+
+        shapes.Add(eSquare);
+        shapes.Add(sRectangle);
+        shapes.Add(eCircle);
+
+        foreach (Shape i in shapes)
+        {
+            double area = i.GetArea();
+            string color = i.GetColor();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
